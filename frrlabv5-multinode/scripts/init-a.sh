d0a2aabd8ec145d6c7ea492034b9e833e12fdb58
Timofei Smolko <timofei.smolko@gmail.com>
2026-03-04 16:28:56 +0000

    added individual startup scripts cause the main one wasnt working

---
+++ frrlabv5-multinode/scripts/init-a.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo ip link del vxlan-ac 2>/dev/null
+sudo ip link del vxlan-at 2>/dev/null
+
+sudo containerlab deploy -t ./vm-a.clab.yml || true
+
+CONTAINER="clab-vm-a-lab-core-a"
+PID=$(docker inspect -f '{{.State.Pid}}' $CONTAINER)
+
+echo "Core PID: $PID"
+
+sudo ip link add vxlan-ac type vxlan id 1002 dev enp0s8 remote 192.168.56.20 dstport 4789
+sudo ip link add vxlan-at type vxlan id 1003 dev enp0s8 remote 192.168.56.40 dstport 4789
+sudo ip link set vxlan-ac up
+sudo ip link set vxlan-at up
+
+sudo ip link set vxlan-ac netns $PID
+sudo ip link set vxlan-at netns $PID
+
+sudo nsenter -t $PID -n ip link set vxlan-ac name eth3
+sudo nsenter -t $PID -n ip link set vxlan-at name eth2
+sudo nsenter -t $PID -n ip link set eth2 up
+sudo nsenter -t $PID -n ip link set eth3 up
+
+sudo nsenter -t $PID -n ip addr add 10.0.2.1/31 dev eth3
+sudo nsenter -t $PID -n ip addr add 10.0.3.0/31 dev eth2
+sudo nsenter -t $PID -n ip addr add 10.255.0.3/32 dev lo
---
+++ frrlabv5-multinode/scripts/init-c.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo ip link del vxlan-ct 2>/dev/null
+sudo ip link del vxlan-ca 2>/dev/null
+
+sudo containerlab deploy -t ./vm-c.clab.yml || true
+
+CONTAINER="clab-vm-c-lab-core-c"
+PID=$(docker inspect -f '{{.State.Pid}}' $CONTAINER)
+
+echo "Core PID: $PID"
+
+sudo ip link add vxlan-ct type vxlan id 1001 dev enp0s8 remote 192.168.56.40 dstport 4789
+sudo ip link add vxlan-ca type vxlan id 1002 dev enp0s8 remote 192.168.56.30 dstport 4789
+sudo ip link set vxlan-ct up
+sudo ip link set vxlan-ca up
+
+sudo ip link set vxlan-ct netns $PID
+sudo ip link set vxlan-ca netns $PID
+
+sudo nsenter -t $PID -n ip link set vxlan-ct name eth2
+sudo nsenter -t $PID -n ip link set vxlan-ca name eth3
+sudo nsenter -t $PID -n ip link set eth2 up
+sudo nsenter -t $PID -n ip link set eth3 up
+
+sudo nsenter -t $PID -n ip addr add 10.0.1.1/31 dev eth2
+sudo nsenter -t $PID -n ip addr add 10.0.2.0/31 dev eth3
+sudo nsenter -t $PID -n ip addr add 10.255.0.2/32 dev lo
